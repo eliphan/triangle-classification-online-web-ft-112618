1fa45c15e288fbcb506462f9327f78a9ef95f9fd
Elise Phan <aliceconnic@gmail.com>
2018-12-13 04:52:16 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,21 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  
+  def initialize(a,b,c)
+   @a = a
+   @b = b
+   @c = c
+  end
+  
+  def kind
+    
+  end
+  
+  
+  
+  
+ 
+  class TriangleError < StandardError
+    # triangle error code
+  end
 end
